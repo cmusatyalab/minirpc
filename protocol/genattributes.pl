5341b15d3f3d58a08351e0bb4c6892293bd59abb
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-04-05 09:37:28 -0400

    Name messages using CHOICE variable name enums
    - Add "Define: " attribute tag, equal to "request" for request PDUs and
    "choice" for top-level message unions
    
    - Rather than building a single table of request PDUs, keyed on the PDU
    definition struct (which isn't very usable), build a table per union type
    which is keyed on the enum values actually contained within the struct
    
    - Enumerate response types by the enum values contained in all "Define:"'d
    union types, rather than by the PDU definition structs

--- protocol/genattributes.pl
+++ protocol/genattributes.pl
@@ -2,6 +2,7 @@
 
 #attributes: is-request
 #
+# Define: {request|choice}
 # Response: {single|multi}
 # Responses: Status Foo
 # Initiators: {client|server}+
@@ -24,6 +25,10 @@ sub validateAttr {
 	my $type;
 	my @vl;
 	
+	if ($attr eq "Define") {
+		$value =~ /^request|choice$/
+			or fail "Invalid definition"
+	}
 	if ($attr eq "Response") {
 		$value =~ /^single|multi$/
 			or fail "Invalid response type";
@@ -45,15 +50,23 @@ sub validateHash {
 	my $attrs = shift;
 	my $type = shift;
 	
-	my @attrlist = ("Response", "Responses", "Initiators");
+	my @attrlist;
 	my $attr;
 	
-	foreach $attr (keys %$attrs) {
-		grep(/^$attr$/, @attrlist)
-			or fail "Unknown attribute specified for $type";
+	fail "Definition not specified, but other attributes are"
+		if (!defined($attrs->{"Define"}));
+	if ($attrs->{"Define"} eq "request") {
+		@attrlist = ("Response", "Responses", "Initiators");
+	} elsif ($attrs->{"Define"} eq "choice") {
+		@attrlist = ()
 	}
 	
-	foreach $attr (@attrlist) {
+	foreach $attr (keys %$attrs) {
+		grep(/^$attr$/, ("Define", @attrlist))
+			or fail "Invalid attribute specified for $type";
+	}
+	
+	foreach $attr ("Define", @attrlist) {
 		fail "$attr attribute not specified for $type"
 			if (!defined($attrs->{$attr}));
 	}
@@ -73,20 +86,46 @@ for $filename (@ARGV) {
 # Stage 2: parse and validate attributes for types that have them
 my $attrs = {};
 my %types;
+my %choicemap;
+my $curchoicemap = ();
+my $choiceName = undef;
 for $filename (@ARGV) {
 	open(FH, "<", $filename)
 		or fail "Can't open $filename";
 	while (<FH>) {
 		if (/^[\t ]*([a-zA-Z0-9]+)[\t ]+::=/) {
+			# Type reference name definition
 			if (%$attrs) {
 				validateHash($attrs, $1);
+				$choiceName = $1
+					if $attrs->{"Define"} eq "choice";
 				$types{$1} = $attrs;
 				$attrs = {};
 			}
+			next;
 		}
-		next if !/^[\t ]*--[\t ]+([a-zA-Z]+)[\t ]*:[\t ]+(.+)$/;
-		validateAttr($1, $2);
-		$attrs->{$1} = $2;
+		if (/^[\t ]*--[\t ]+([a-zA-Z]+)[\t ]*:[\t ]+(.+)$/) {
+			# Attribute definition
+			validateAttr($1, $2);
+			$attrs->{$1} = $2;
+		}
+		if ($choiceName) {
+			if (/SEQUENCE|SET|CHOICE|{/) {
+				fail "Nested definitions not supported " .
+						"in choices with \"Define: " .
+						"choice\" set"
+			}
+			if (/^[\t ]*([a-zA-Z0-9-]+)[\t ]+([a-zA-Z0-9-]+),/) {
+				# Choice member
+				$curchoicemap->{$2} = $1;
+			}
+			if (/}/) {
+				# End of choice
+				$choicemap{$choiceName} = $curchoicemap;
+				$curchoicemap = ();
+				$choiceName = undef;
+			}
+		}
 	}
 	close FH;
 }
@@ -99,10 +138,10 @@ print <<EOF;
 #define SERVER 0x2
 
 struct validate_entry {
-	asn_TYPE_descriptor_t *type;
+	int type;
 	int multi;
 	unsigned initiators;
-	asn_TYPE_descriptor_t **response_types;
+	int response_types[];
 };
 
 EOF
@@ -110,25 +149,36 @@ EOF
 my $responses;
 my $type;
 my $rtype;
+my $typevar;
 while (($type, $attrs) = each %types) {
+	next if $attrs->{"Define"} ne "request";
 	undef $responses;
 	foreach $rtype (split(/[\t ]+/, $attrs->{"Responses"})) {
-		$responses .= "&asn_DEF_$rtype, ";
+		while (($choiceName, $curchoicemap) = each %choicemap) {
+			if (defined($curchoicemap->{$rtype})) {
+				$typevar = $curchoicemap->{$rtype};
+				$responses .= "${choiceName}_PR_$typevar, "
+			}
+		}
 	}
-	print "asn_TYPE_descriptor_t *${type}_responses[] =\n";
-	print "{${responses}NULL};\n\n";
+	print "int ${type}_responses[] =\n";
+	print "{${responses}0};\n\n";
 }
 
 my $multi;
 my $initiator;
 my $initiators;
-print "struct validate_entry validate_entries[] = {\n";
-while (($type, $attrs) = each %types) {
-	$multi = ($attrs->{"Response"} eq "multi");
-	$initiators = "0";
-	foreach $initiator (split(/[\t ]+/, $attrs->{"Initiators"})) {
-		$initiators .= "|" . uc $initiator;
+while (($choiceName, $curchoicemap) = each %choicemap) {
+	print "struct validate_entry validate_entries_${choiceName}[] = {\n";
+	while (($type, $typevar) = each %$curchoicemap) {
+		next if !defined($types{$type});
+		$attrs = $types{$type};
+		$multi = ($attrs->{"Response"} eq "multi");
+		$initiators = "0";
+		foreach $initiator (split(/[\t ]+/, $attrs->{"Initiators"})) {
+			$initiators .= "|" . uc $initiator;
+		}
+		print "{${choiceName}_PR_$typevar, $multi, $initiators, ${type}_responses},\n"
 	}
-	print "{&asn_DEF_$type, $multi, $initiators, ${type}_responses},\n"
+	print "{0, 0, 0, NULL}\n};\n";
 }
-print "{NULL, 0, 0, NULL}\n};\n";
