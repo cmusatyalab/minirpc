bd8cb455acdfae965276de541ea064b072a78073
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2007-05-31 12:43:52 -0400

    Add/update Eclipse license blocks

--- minirpcgen.pl
+++ minirpcgen.pl
@@ -1,5 +1,16 @@
 #!/usr/bin/perl
 
+#
+# miniRPC - TCP RPC library with asynchronous operations and TLS support
+#
+# Copyright (C) 2007 Carnegie Mellon University
+#
+# This software is distributed under the terms of the Eclipse Public License,
+# Version 1.0 which can be found in the file named LICENSE.  ANY USE,
+# REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES RECIPIENT'S
+# ACCEPTANCE OF THIS AGREEMENT
+#
+
 use strict;
 use warnings;
 use Getopt::Std;
--- xdr_len.c
+++ xdr_len.c
@@ -1,3 +1,14 @@
+/*
+ * miniRPC - TCP RPC library with asynchronous operations and TLS support
+ *
+ * Copyright (C) 2007 Carnegie Mellon University
+ *
+ * This software is distributed under the terms of the Eclipse Public License,
+ * Version 1.0 which can be found in the file named LICENSE.  ANY USE,
+ * REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THIS AGREEMENT
+ */
+
 #include <rpc/xdr.h>
 #define MINIRPC_INTERNAL
 #include "internal.h"
