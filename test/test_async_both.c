c56c1ad375c3e8c062d8e8ea10abc52d15d0b7ce
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2008-02-26 16:16:58 -0500

    test: Add test which uses both async server and client

---
+++ test/test_async_both.c
@@ -0,0 +1,55 @@
+/*
+ * miniRPC - TCP RPC library with asynchronous operations and TLS support
+ *
+ * Copyright (C) 2007-2008 Carnegie Mellon University
+ *
+ * This software is distributed under the terms of the Eclipse Public License,
+ * Version 1.0 which can be found in the file named LICENSE.  ANY USE,
+ * REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#include <unistd.h>
+#include "common.h"
+
+static const struct mrpc_config client_config = {
+	.protocol = &proto_client,
+	.disconnect = disconnect_fatal
+};
+
+static const struct mrpc_config server_config = {
+	.protocol = &proto_server,
+	.accept = async_server_accept,
+	.disconnect = disconnect_fatal
+};
+
+int main(int argc, char **argv)
+{
+	struct mrpc_conn_set *sset;
+	struct mrpc_conn_set *cset;
+	struct mrpc_connection *conn;
+	int port;
+	int ret;
+
+	if (mrpc_init())
+		die("Couldn't initialize minirpc");
+	async_server_init();
+	async_client_init();
+	sset=spawn_server(&port, &server_config, NULL, 1);
+
+	if (mrpc_conn_set_alloc(&cset, &client_config, NULL))
+		die("Couldn't allocate conn set");
+
+	ret=mrpc_connect(&conn, cset, "localhost", port, NULL);
+	if (ret)
+		die("%s", strerror(-ret));
+
+	launch_dispatch_thread(cset);
+	async_client_set_ops(conn);
+	async_client_run(conn);
+	trigger_callback_sync(conn);
+	/* Give the async client some additional time to notice if it receives
+	   more callbacks than it should */
+	sleep(1);
+	return 0;
+}
