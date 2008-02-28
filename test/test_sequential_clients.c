c1417a350e6717f5e4b08675105b35aa129e4ccc
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2008-02-28 11:44:12 -0500

    test: Add sequential-client test
    - Add test that makes multiple client connections in a row

---
+++ test/test_sequential_clients.c
@@ -0,0 +1,50 @@
+/*
+ * miniRPC - TCP RPC library with asynchronous operations and TLS support
+ *
+ * Copyright (C) 2007-2008 Carnegie Mellon University
+ *
+ * This software is distributed under the terms of the Eclipse Public License,
+ * Version 1.0 which can be found in the file named LICENSE.  ANY USE,
+ * REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES RECIPIENT'S
+ * ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#include "common.h"
+
+static const struct mrpc_config client_config = {
+	.protocol = &proto_client,
+	.disconnect = disconnect_fatal
+};
+
+static const struct mrpc_config server_config = {
+	.protocol = &proto_server,
+	.accept = sync_server_accept,
+	.disconnect = disconnect_normal
+};
+
+int main(int argc, char **argv)
+{
+	struct mrpc_conn_set *sset;
+	struct mrpc_conn_set *cset;
+	struct mrpc_connection *conn;
+	int port;
+	int ret;
+	int i;
+
+	if (mrpc_init())
+		die("Couldn't initialize minirpc");
+	sset=spawn_server(&port, &server_config, NULL, 1);
+
+	if (mrpc_conn_set_alloc(&cset, &client_config, NULL))
+		die("Couldn't allocate conn set");
+	launch_dispatch_thread(cset);
+
+	for (i=0; i<200; i++) {
+		ret=mrpc_connect(&conn, cset, "localhost", port, NULL);
+		if (ret)
+			die("%s", strerror(-ret));
+		sync_client_set_ops(conn);
+		sync_client_run(conn);
+	}
+	return 0;
+}
