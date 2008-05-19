f2010806df04eed059f08c544ba35dc5afc4234b
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2008-05-19 17:02:29 -0400

    test: Remove sched_yield() from test_simultaneous_close
    - Remove sched_yield() calls from test_simultaneous_close, since
    sched_yield() doesn't always yield.  Use sem_wait() calls on alternate
    iterations instead.
    
    - Force the server event thread to synchronize before closing the
    connection, to ensure that the client sometimes gets a chance to close it
    first.  Otherwise, with the CFS CPU scheduler, the server always wins.
    
    - Don't fail if the client or server has a close count of zero (matching the
    error message)

--- test/test_simultaneous_close.c
+++ test/test_simultaneous_close.c
@@ -9,19 +9,22 @@
  */
 
 #define ITERS 100
-#define YIELDCOUNT 5
 
-#include <sched.h>
+#include <semaphore.h>
 #include <glib.h>
 #include "common.h"
 
+sem_t serv_can_close;
+sem_t serv_closed;
 gint server_user_disc;
 gint client_user_disc;
 
 void *do_accept(void *set_data, struct mrpc_connection *conn,
 			struct sockaddr *from, socklen_t from_len)
 {
+	sem_wait(&serv_can_close);
 	expect(mrpc_conn_close(conn), 0);
+	sem_post(&serv_closed);
 	mrpc_conn_unref(conn);
 	return conn;
 }
@@ -57,10 +60,11 @@ int main(int argc, char **argv)
 	char *port;
 	int ret;
 	int i;
-	int j;
 	int client;
 	int server;
 
+	sem_init(&serv_can_close, 0, 0);
+	sem_init(&serv_closed, 0, 0);
 	sset=spawn_server(&port, proto_server, do_accept, NULL, 1);
 	mrpc_set_disconnect_func(sset, server_disconnect);
 
@@ -73,15 +77,18 @@ int main(int argc, char **argv)
 		ret=mrpc_conn_create(&conn, cset, NULL);
 		if (ret)
 			die("%s", strerror(ret));
+		if (i % 2)
+			sem_post(&serv_can_close);
 		ret=mrpc_connect(conn, AF_UNSPEC, "localhost", port);
 		if (ret)
 			die("%s", strerror(ret));
-
-		if (i % 2)
-			for (j=0; j<YIELDCOUNT; j++)
-				sched_yield();
-
+		if (i % 2) {
+			sem_wait(&serv_closed);
+			sem_wait(&serv_closed);
+		}
 		expect(mrpc_conn_close(conn), 0);
+		if (!(i % 2))
+			sem_post(&serv_can_close);
 		mrpc_conn_unref(conn);
 	}
 
@@ -91,9 +98,9 @@ int main(int argc, char **argv)
 	dispatcher_barrier();
 	client=g_atomic_int_get(&client_user_disc);
 	server=g_atomic_int_get(&server_user_disc);
-	if (client == 0 || client > ITERS)
+	if (client < 0 || client > ITERS)
 		die("Client close count = %d, expected 0-%d", client, ITERS);
-	if (server == 0 || server > ITERS)
+	if (server < 0 || server > ITERS)
 		die("Server close count = %d, expected 0-%d", server, ITERS);
 	free(port);
 	return 0;
